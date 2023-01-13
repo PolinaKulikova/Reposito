2da7e1db7975ffb66d233e3ecf18e2f895f298c8
Polina Kulikova <pomidorishna@gmail.com>
2023-01-13 21:47:02 +0300

    Initual commit

---
+++ Ex11/Program.cs
@@ -0,0 +1,14 @@
+﻿void FillArray(int[] collection)      // Метод заполнения массива случайными числами от 1 до 10
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);             Положить случайное число от 1 до 10 в каждый элемент массива
+        index++;
+    }
+}
+
+
+int[] array = new int[10];   // new int[10] создание случайного массива из 10 элементов int = 0  
+
---
+++ Ex9/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти максимум из элементов массива
+
+int[] array = { 11, 21, 3111, 41, 15, 61, 17, 18, 19 };
+
+int Max(int arg1, int arg2, int arg3) // метод нахождения максимума с разбивкой на 3 группы
+{
+    int result = arg1;
+    if (result < arg2) result = arg2; 
+    if (result < arg3) result = arg3; 
+    return result;
+}
+ int max = Max(                          // назначаем переменной  max максимальное значение
+    Max(array[0], array[1], array[2]), 
+    Max(array[3], array[4], array[5]), 
+    Max(array[6], array[7], array[8]));
+
+Console.WriteLine(max);
