cc9a3ba6bf05eb1f565edd8ffff611c75500118a
Polina Kulikova <pomidorishna@gmail.com>
2023-01-13 21:47:51 +0300

    Initual commit11

--- Ex11/Program.cs
+++ Ex11/Program.cs
@@ -4,7 +4,7 @@
     int index = 0;
     while (index < length)
     {
-        collection[index] = new Random().Next(1, 10);             Положить случайное число от 1 до 10 в каждый элемент массива
+        collection[index] = new Random().Next(1, 10);             //Положить случайное число от 1 до 10 в каждый элемент массива
         index++;
     }
 }
