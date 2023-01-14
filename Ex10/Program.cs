7a7020e46f38566e20d80f21e66d6748b90177c6
Polina Kulikova <pomidorishna@gmail.com>
2023-01-14 22:03:54 +0300

    Initual commit231

--- Ex10/Program.cs
+++ Ex10/Program.cs
@@ -6,17 +6,43 @@
 
 int[] array = { 1, 12, 31, 4, 15, 16, 17, 18};
 int n = array.Length;
-int find = 18;
+// int find = 18;
 int index = 0;
 
-while (index<n)                    // Нахождение индекса искомого заранее известного элемента find
-{
+// while (index<n)                    // Нахождение индекса искомого заранее известного элемента find
+// {
     
-    if ((array[index] == find))
+//     if ((array[index] == find))
+//     {
+//         Console.WriteLine(index);
+//         break;                      // Если таких значений в массиве несколько, прервать алгоритм после нахождения первого
+//     } 
+//     index = index+1;
+// }
+                                                    // Метод нахождения индекса искомого элемента в массиве   
+int IndexOf(int[] collection, int find)      
+{
+    int count = collection.Length;                 
+    int index = 0;
+    int position = -1;                              // Значение -1 считаем ошибкой, если значение не будет найдено в массиве, программа выдаст -1, а не ноль
+    while (index<count)
     {
-        Console.WriteLine(index);
-        break;                      // Если таких значений в массиве несколько, прервать алгоритм после нахождения первого
-    } 
-    index = index+1;
+    if (collection[index] == find)
+    {
+        position = index;
+        break;                             //Перестать искать после находжения первого
+    }
+        index++;
+    }
+    return position;
+}    
+
+double inputValue()                                                //Метод проверки на дурака               
+{
+    double result;
+    while (!double.TryParse(Console.WriteLine(), out result));
+    return result;
 }
 
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
--- Ex11/Program.cs
+++ Ex11/Program.cs
@@ -1,4 +1,4 @@
-﻿void FillArray(int[] collection)      // Метод заполнения массива случайными числами от 1 до 10
+﻿ void FillArray(int[] collection)      // Метод заполнения массива случайными числами от 1 до 10
 {
     int length = collection.Length;
     int index = 0;
@@ -9,6 +9,19 @@
     }
 }
 
+void PrintArray(int[] collection)     // Метод печати массива
+{
+    int count = collection.Length;
+    int position = 0;
+    while (position<count)
+    {
+        Console.WriteLine(collection[position]);
+        position++; 
+     }
+} 
+
 
 int[] array = new int[10];   // new int[10] создание случайного массива из 10 элементов int = 0  
 
+FillArray(array);                     // Вызов метода заполнения  массива
+PrintArray(array);                    // Вызов метода печати массива
